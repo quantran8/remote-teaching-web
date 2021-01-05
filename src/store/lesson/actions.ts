5b5cb100f1aa97b90328c40e84220531f5cc3a5e
jackpham07 <jack.pham@grapecity.com>
2021-01-05 14:54:47 +0700

    Fix content signature

--- src/store/lesson/actions.ts
+++ src/store/lesson/actions.ts
@@ -34,9 +34,17 @@ interface LessonActions<S, R>
     LessonActionsInterface<S, R> {}
 
 const actions: LessonActions<LessonState, any> = {
-  setInfo(store: ActionContext<LessonState, any>, payload: LessonPlanModel) {
+  async setInfo(
+    store: ActionContext<LessonState, any>,
+    payload: LessonPlanModel
+  ) {
     if (!payload) return;
-    const signalture = store.rootGetters["contentSignature"];
+    let signalture = store.rootGetters["contentSignature"];
+    if (!signalture) {
+      await store.dispatch("loadContentSignature",{},{root: true});
+      signalture = store.rootGetters["contentSignature"];
+    }
+    console.log("signature", signalture);
     const exposures: Array<Exposure> = payload.contents.map((e) => {
       const items: Array<ExposureItem> = e.contents.map((c) => {
         const media: Array<ExposureItemMedia> = c.page.map((p) => {
