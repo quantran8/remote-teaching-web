3a5b025740c5afd8f0542a2422347ad1962aec07
loganle <logan.le@grapecity.com>
2021-04-09 09:23:10 +0700

    revert 8910

--- src/views/teacher-home/teacher-home.vue
+++ src/views/teacher-home/teacher-home.vue
@@ -5,32 +5,18 @@
     <p>Choose a class to start</p>
     <hr />
     <div>
-      <div class="teacher-page__school-select">
-        <Select
-          placeholder="School"
-          showSearch
-          :disabled="disabled"
-          :loading="loading"
-          :value="schools[0]?.id"
-          :filterOption="false"
-          @search="filterSchools"
-          @change="onSchoolChange"
-        >
-          <Option :value="school.id" :key="school.id" v-for="school in filteredSchools">{{ school.name }}</Option>
-        </Select>
-      </div>
-      <div class="teacher-page__classgroup">
-        <ClassGroupItem
-          v-for="cl in classes"
-          :key="cl.schoolClassId"
-          :schoolClass="cl"
-          :onClickClass="(cls) => onClickClass(cls)"
-          :onClickGroup="(classId) => onClickGroup(classId)"
-          :canStartSession="canStartSession"
-        />
-      </div>
+      <ClassCard
+        class="card-margin"
+        v-for="cl in classes"
+        :key="cl.schoolClassId"
+        :id="cl.schoolClassId"
+        :title="cl.schoolClassName"
+        :description="cl.schoolName"
+        :active="cl.isActive"
+        @click="() => onClickClass(cl)"
+      />
     </div>
   </div>
 </template>
-<style lang="less" scoped src="./teacher-home.less"></style>
+<style lang="scss" scoped src="./teacher-home.scss"></style>
 <script lang="ts" src="./teacher-home.ts"></script>
