bdc2a6631ae256ee1157eabc8cd42d4a6e9d66d6
Charlie Tran <CharlieTran@GRAPECITY.NET>
2021-04-26 17:27:24 +0700

    remove unnecessary student-tools component

--- src/components/common/annotation-view/student-tools/student-tools.vue
+++
@@ -1,7 +0,0 @@
-<template>
-	<div class="student-tools">
-		Tools Here
-	</div>
-</template>
-<style lang="scss" scoped src="./student-tools.scss"></style>
-<script lang="ts" src="./student-tools.ts"></script>
