ccf72fa6014ffad7d41bc95cb5e1c2e24f2243d9
jackpham07 <jack.pham@grapecity.com>
2021-05-19 10:50:06 +0700

    GL-9689: update remove microphone trach play

--- src/agora/index.ts
+++ src/agora/index.ts
@@ -118,11 +118,10 @@ export class AgoraClient implements AgoraClientSDK {
   async openMicrophone(): Promise<any> {
     if (this._microphoneTrack) return;
     try {
-      this._microphoneTrack = await this.agoraRTC.createMicrophoneAudioTrack({AEC: true, ANS: true});
+      this._microphoneTrack = await this.agoraRTC.createMicrophoneAudioTrack();
       this.microphoneTrack.on("track-ended", () => {
         this.microphoneTrack && this._closeMediaTrack(this.microphoneTrack);
       });
-      this.microphoneTrack.play();
       this.microphoneError = null;
     } catch (err) {
       this.microphoneError = err;
