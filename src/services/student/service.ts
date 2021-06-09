3c74a693d6f3f151c57ee293179bc5f08cc89926
Marcus Nguyen <marcus.nguyen@grapecity.com>
2021-06-09 13:38:48 +0700

    Change Api

--- src/services/student/service.ts
+++ src/services/student/service.ts
@@ -1,7 +1,7 @@
 import { AdminService } from "../admin.service";
 class GLStudentService extends AdminService {
   getAvatarTeacher(teacherId: string): Promise<any> {
-    return this.get(`schools/teachers/${teacherId}/avatar`);
+    return this.get(`schools/teacher/${teacherId}/avatar`);
   }
   getAvatarStudent(studentId: string): Promise<any> {
     return this.get(`students/${studentId}/avatar`);
