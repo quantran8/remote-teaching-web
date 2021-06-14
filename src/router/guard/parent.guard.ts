1558249d0d5eb3a3695d17baaf0d3097875fb1df
Charlie Tran <CharlieTran@GRAPECITY.NET>
2021-06-14 09:16:21 +0700

    Revert "added remote tsi settings check"
    This reverts commit 56bef53edbe41d5c7a536d521cab0cece7e6487a.

--- src/router/guard/parent.guard.ts
+++ src/router/guard/parent.guard.ts
@@ -1,16 +1,21 @@
 import { NavigationGuardNext, RouteLocationNormalized } from "vue-router";
 import { AuthService, LoginInfo, RoleName } from "@/commonui";
+import { RequireParentError } from "../error";
 
-export default (to: RouteLocationNormalized, _from: RouteLocationNormalized, _next: NavigationGuardNext) => {
-  const requireParent: boolean = to.matched.some(record => record.meta.requireParent);
-  if (!requireParent) return true;
+export default (
+  to: RouteLocationNormalized,
+  _from: RouteLocationNormalized,
+  _next: NavigationGuardNext
+) => {
+  const requireParent: boolean = to.matched.some(
+    (record) => record.meta.requireParent
+  );
+  if (!requireParent) return;
   const loginInfo: LoginInfo = AuthService.getLoginInfo();
   if (loginInfo && loginInfo.profile) {
     const isParent = loginInfo.profile.roles.indexOf(RoleName.parent) !== -1;
-    return isParent;
-  }
-  else
-  {
-	  return false;
+    if (!isParent) {
+      throw new RequireParentError();
+    }
   }
 };
