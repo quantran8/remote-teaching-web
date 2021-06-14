b27508a0cd320df4b433ea06b73cd8cf32ef5287
Charlie Tran <CharlieTran@GRAPECITY.NET>
2021-06-14 11:07:09 +0700

    remove langs not support

--- src/components/common/language-picker/constants.ts
+++ src/components/common/language-picker/constants.ts
@@ -3,22 +3,14 @@ export const languages = [
     value: "en",
     label: "English",
   },
-//   {
-//     value: "ar-sa",
-//     label: "العربية",
-//   },
-//   {
-//     value: "es",
-//     label: "Español",
-//   },
   {
     value: "zh-cn",
     label: "中文",
   },
-//   {
-//     value: "ms",
-//     label: "中文(马来西亚)",
-//   },
+  {
+    value: "ml",
+    label: "中文(马来西亚)",
+  },
   {
     value: "js",
     label: "日本語",
