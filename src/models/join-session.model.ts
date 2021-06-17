8e993f00437ea4c6944a870bc6a8907e531a22cc
loganle <logan.le@grapecity.com>
2021-06-17 10:54:33 +0700

    update logic to join/start session

---
+++ src/models/join-session.model.ts
@@ -0,0 +1,9 @@
+export interface JoinSession {
+  classId: string,
+  groupId: string,
+  browser: string,
+  device: string,
+  bandwidth: string,
+  resolution: string,
+  browserFingerprint: string
+}
