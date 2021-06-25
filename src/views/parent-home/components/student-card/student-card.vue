1dc94f8f608fcb1555b3b1c66243e58a2a853f1d
Marcus Nguyen <marcus.nguyen@grapecity.com>
2021-06-25 10:48:38 +0700

    Add Indicator

--- src/views/parent-home/components/student-card/student-card.vue
+++ src/views/parent-home/components/student-card/student-card.vue
@@ -2,8 +2,18 @@
   <BaseCard class="user-container">
     <img class="user-avatar" :src="userAvatar" />
     <div class="user-name">{{ name }}</div>
-    <div class="next-session mt-20">Class: {{ nextSessionInfo && nextSessionInfo.classInfo.className }}</div>
-    <div class="next-session">Next Session: {{ nextSessionInfo && convertDate(nextSessionInfo.nextTime) }}</div>
+    <div class="next-session-info-online mt-20" v-if="nextSessionInfo && nextSessionInfo.sessionId">
+      Class: {{ nextSessionInfo && nextSessionInfo.classInfo.className }}
+      <div class="indicator-container">
+        <span class="indicator-container__out">
+          <span class="indicator-container__in"></span>
+        </span>
+      </div>
+    </div>
+    <div class="next-session-info mt-20" v-else>
+      <p class="next-session-info__class">Class: {{ nextSessionInfo && nextSessionInfo.classInfo.className }}</p>
+      <p class="next-session-info__time">Next Session: {{ nextSessionInfo && convertDate(nextSessionInfo.nextTime) }}</p>
+    </div>
   </BaseCard>
 </template>
 <style lang="scss" scoped src="./student-card.scss"></style>
