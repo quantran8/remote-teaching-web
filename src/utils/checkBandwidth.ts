97b895d1981b7a9bf9c6fa17a5bfc78850fc3a00
Marcus Nguyen <marcus.nguyen@grapecity.com>
2021-07-06 15:24:02 +0700

    update code

--- src/utils/checkBandwidth.ts
+++ src/utils/checkBandwidth.ts
@@ -1,28 +1,25 @@
 import { RemoteTeachingService } from "@/services";
 
-const downloadSize = 264422; //bytes
+const imageAddr = "/img/checkBandwidthImage.png";
+const downloadSize = 3444; //kb
 const download = new Image();
 
 //300000 = 5 minutes;
 export const checkBandwidth = async (studentId?: string) => {
   setInterval(() => {
     const startTime = new Date().getTime();
-    const imageAddr = "./src/assets/images/checkBandwidthImage.png";
-    const cacheBuster = "&nnn=" + startTime;
     download.onload = function() {
       const endTime = new Date().getTime();
       const duration = (endTime - startTime) / 1000;
       const bitsLoaded = downloadSize * 8;
-      const speedBps = +(bitsLoaded / duration).toFixed(2);
-      const speedKbps = +(speedBps / 1024).toFixed(2);
+      const speedKbps = +(bitsLoaded / duration).toFixed(2);
       const speedMbps = +(speedKbps / 1024).toFixed(2);
-      console.log(speedMbps);
       if (studentId) {
         RemoteTeachingService.putStudentBandwidth(studentId, Math.round(speedMbps).toString());
       } else {
         RemoteTeachingService.putTeacherBandwidth(Math.round(speedMbps).toString());
       }
     };
-    download.src = imageAddr + cacheBuster;
+    download.src = imageAddr;
   }, 10000);
 };
