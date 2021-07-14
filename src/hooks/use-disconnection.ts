7d7b0e8119852c07c850ced25d52f6336a4802ca
Charlie Tran <CharlieTran@GRAPECITY.NET>
2021-07-14 09:41:38 +0700

    clear some code

--- src/hooks/use-disconnection.ts
+++ src/hooks/use-disconnection.ts
@@ -226,4 +226,11 @@ export const useDisconnection = () => {
       }
     }
   });
+
+  watch(route, currentRoute => {
+    const classRoomStatus: number = getters["classRoomStatus"];
+    if (currentRoute.params.classId && classRoomStatus === ClassRoomStatus.InClass) {
+      dispatch("setClassRoomStatus", { status: ClassRoomStatus.InDashBoard });
+    }
+  });
 };
