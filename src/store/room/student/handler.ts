5701e7c81e9cf564dec6c79dc015177d44df2165
Charlie Tran <CharlieTran@GRAPECITY.NET>
2021-07-30 22:04:25 +0700

    Revert "add some log"
    This reverts commit 19af57ebc1684f9d513b7147edf235735e0ee4ef.

--- src/store/room/student/handler.ts
+++ src/store/room/student/handler.ts
@@ -56,8 +56,6 @@ export const useStudentRoomHandler = (store: ActionContext<StudentRoomState, any
       }
     },
     onStudentJoinClass: (payload: StudentModel) => {
-		console.log("STUDENTSIDE: teacher join class", payload);
-
       commit("setStudentStatus", {
         id: payload.id,
         status: payload.connectionStatus,
@@ -106,7 +104,6 @@ export const useStudentRoomHandler = (store: ActionContext<StudentRoomState, any
       dispatch("updateAudioAndVideoFeed", {});
     },
     onTeacherJoinClass: (payload: TeacherModel) => {
-		console.log("STUDENTSIDE: teacher join class", payload);
       commit("setTeacherDisconnected", false);
       commit("setTeacherStatus", {
         id: payload.id,
