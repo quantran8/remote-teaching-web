95c984171daf10b23190a3bb3b9b0957899550db
jackpham07 <jack.pham@grapecity.com>
2021-08-04 08:49:02 +0700

    update

--- src/views/student-class/student-class.ts
+++ src/views/student-class/student-class.ts
@@ -218,8 +218,6 @@ export default defineComponent({
           joinLoading.value = false;
         } else if (apiStatus.value.code === GLErrorCode.CLASS_HAS_BEEN_ENDED) {
           await router.push(Paths.Parent);
-          showMessage.value = true;
-          joinLoading.value = false;
         } else if (apiStatus.value.code === GLErrorCode.PARENT_NOT_HAVE_THIS_STUDENT) {
           showMessage.value = true;
           joinLoading.value = false;
