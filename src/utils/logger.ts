b273aef9a3179b1012882418388f7d6fe89793c4
Marcus Nguyen <marcus.nguyen@grapecity.com>
2021-08-20 11:11:14 +0700

    Revert Function

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -2,18 +2,18 @@ export class Logger {
   private static _DISABLE_ = process.env.VUE_APP_ENVIRONMENT === "prod";
   public static log(message?: any, ...optionalParams: any[]) {
     if (this._DISABLE_) return;
-    Logger.log(message ? message : "", ...optionalParams);
+    console.log(message ? message : "", ...optionalParams);
   }
   public static info(message?: any, ...optionalParams: any[]) {
     if (this._DISABLE_) return;
-    Logger.info(message ? message : "", ...optionalParams);
+    console.info(message ? message : "", ...optionalParams);
   }
   public static error(message?: any, ...optionalParams: any[]) {
     if (this._DISABLE_) return;
-    Logger.error(message ? message : "", ...optionalParams);
+    console.error(message ? message : "", ...optionalParams);
   }
   public static debug(message?: any, ...optionalParams: any[]) {
     if (this._DISABLE_) return;
-    Logger.debug(message ? message : "", ...optionalParams);
+    console.debug(message ? message : "", ...optionalParams);
   }
 }
