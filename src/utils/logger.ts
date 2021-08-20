fd9f1cff4d305648206e38592869766e9f44bee5
Marcus Nguyen <marcus.nguyen@grapecity.com>
2021-08-20 13:28:16 +0700

    Update condition

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,19 +1,28 @@
 export class Logger {
-  private static _DISABLE_ = localStorage.getItem("prod_log_enable") === "false" || process.env.VUE_APP_ENVIRONMENT === "prod";
+  private static disable = () => {
+    if (process.env.VUE_APP_ENVIRONMENT === "prod") {
+      let status = true;
+      if (localStorage.getItem("prod_log_enable") === "true") {
+        status = false;
+      }
+      return status;
+    }
+    return false;
+  };
   public static log(message?: any, ...optionalParams: any[]) {
-    if (this._DISABLE_) return;
+    if (this.disable()) return;
     console.log(message ? message : "", ...optionalParams);
   }
   public static info(message?: any, ...optionalParams: any[]) {
-    if (this._DISABLE_) return;
+    if (this.disable()) return;
     console.info(message ? message : "", ...optionalParams);
   }
   public static error(message?: any, ...optionalParams: any[]) {
-    if (this._DISABLE_) return;
+    if (this.disable()) return;
     console.error(message ? message : "", ...optionalParams);
   }
   public static debug(message?: any, ...optionalParams: any[]) {
-    if (this._DISABLE_) return;
+    if (this.disable()) return;
     console.debug(message ? message : "", ...optionalParams);
   }
 }
