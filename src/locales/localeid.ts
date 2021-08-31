deb4973d7f827be70e46d1d8fbd693059809caa8
GRAPECITY\tysontrinh <tyson.trinh@grapecity.com>
2021-08-31 09:51:27 +0700

    update locale

--- src/locales/localeid.ts
+++ src/locales/localeid.ts
@@ -196,6 +196,11 @@ export class TeacherHome {
   static Next = "teacher_home.next";
 }
 
+export class TeacherClass {
+	static LeavePage = "teacher_class.leave_page";
+	static MarkAsComplete = "teacher_class.mark_as_complete";
+}
+
 export class LostNetwork {
   static Message = "lost_network.message";
   static StudentMessage = "lost_network.student_message";
