601f6e4faf4809f9c78bda3613f5f3acac7c87b4
jackpham07 <jack.pham@grapecity.com>
2021-11-16 14:46:24 +0700

    GL-11351: update whiteboard when student join the class and had another student in mode one one

--- src/components/common/annotation-view/annotation-view.ts
+++ src/components/common/annotation-view/annotation-view.ts
@@ -74,17 +74,30 @@ export default defineComponent({
     });
     const { processPushShapes, addStar, addCircle, addSquare } = studentAddedShapes();
     const { processAnnotationLesson } = annotationCurriculumStudent();
+    const hideWhiteboard = () => {
+      canvas.setBackgroundColor("transparent", canvas.renderAll.bind(canvas));
+      toolActive.value = "";
+      canvas.isDrawingMode = false;
+      processAnnotationLesson(canvas, props.image, containerRef, isShowWhiteBoard);
+    };
+    const processCanvasWhiteboardSetup = () => {
+      if (isShowWhiteBoard.value) {
+        canvas.remove(...canvas.getObjects().filter((obj: any) => obj.id === "annotation-lesson"));
+        canvas.setBackgroundColor("white", canvas.renderAll.bind(canvas));
+      } else {
+        hideWhiteboard();
+      }
+    };
     const processCanvasWhiteboard = () => {
       if (isShowWhiteBoard.value) {
         canvas.remove(...canvas.getObjects().filter((obj: any) => obj.id === "annotation-lesson"));
-        if (!studentOneAndOneId.value || student.value.id == studentOneAndOneId.value) {
+        if (studentOneAndOneId.value && student.value.id == studentOneAndOneId.value) {
+          canvas.setBackgroundColor("white", canvas.renderAll.bind(canvas));
+        } else {
           canvas.setBackgroundColor("white", canvas.renderAll.bind(canvas));
         }
       } else {
-        canvas.setBackgroundColor("transparent", canvas.renderAll.bind(canvas));
-        toolActive.value = "";
-        canvas.isDrawingMode = false;
-        processAnnotationLesson(canvas, props.image, containerRef, isShowWhiteBoard);
+        hideWhiteboard();
       }
     };
     watch(isShowWhiteBoard, () => {
@@ -340,6 +353,7 @@ export default defineComponent({
       });
       listenToCanvasEvents();
       resizeCanvas();
+      processCanvasWhiteboardSetup();
     };
     const imgLoad = () => {
       processAnnotationLesson(canvas, props.image, containerRef, isShowWhiteBoard);
