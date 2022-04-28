3a57f3234f881540dc3c0c6c22b1ef8766ca0ed4
tyson.trinh <tyson.trinh@grapecity.com>
2022-04-28 11:58:20 +0700

    add config for weblocal with latest v8.5 source code

--- src/store/auth/actions.ts
+++ src/store/auth/actions.ts
@@ -7,6 +7,7 @@ const actions: ActionTree<AuthState, any> = {
     store.commit("setLoginInfo", payload.loginInfo);
   },
   signout(store, _) {
+	return;
     store.commit("setLoginInfo", null);
   },
 };
