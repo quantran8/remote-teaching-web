a58a5376b7570b891ca07c7ae4d54cde8050d110
Joe's LAB <joevu98@gmail.com>
2022-07-14 17:37:16 +0700

    update position tooltip

--- src/views/teacher-class/components/lesson-plan/exposure-detail/exposure-item/exposure-item.vue
+++ src/views/teacher-class/components/lesson-plan/exposure-detail/exposure-item/exposure-item.vue
@@ -9,7 +9,7 @@
         :key="item.id"
         :class="{ 'item-active': item.id === currentExposureItemMedia?.id, 'content-block': isContent }"
       >
-        <Tooltip placement="topLeft" :overlayStyle="{ maxWidth: '400px'}">
+        <Tooltip placement="rightBottom" :overlayStyle="{ maxWidth: '400px'}">
           <template v-if="isTeaching" #title>
             <span v-html="item?.teachingContent"></span>
           </template>
