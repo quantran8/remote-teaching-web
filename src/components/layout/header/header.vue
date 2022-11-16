e6d79cd1b67f0bfa083067056cb45bd3805240ec
Joe's LAB <joevu98@gmail.com>
2022-11-16 11:02:39 +0700

    fix writer review not showing in Calendar page, localizer writer review text

--- src/components/layout/header/header.vue
+++ src/components/layout/header/header.vue
@@ -1,18 +1,19 @@
 <template>
-  <AppHeader class="header" :title="title">
-    <template #right>
-      <ResourceMenu />
-      <HelpMenu />
-      <LanguagePicker />
-      <UserAvatar>
-        <template #extraItems>
-          <DropdownItem :text="testConnectText" @click.prevent="onClickTestDevice" />
-          <DropdownItem v-if="isShowWriterReview" text="Writer Review" @click.prevent="onClickWriterReview" />
-        </template>
-      </UserAvatar>
-    </template>
-    <DeviceTester :notJoin="true" ref="deviceTesterRef" />
-  </AppHeader>
-</template>
-<style src="./header.scss" lang="scss" scoped></style>
-<script src="./header.ts" lang="ts"></script>
+	<AppHeader class="header" :title="title">
+	  <template #right>
+		<ResourceMenu />
+		<HelpMenu />
+		<LanguagePicker />
+		<UserAvatar>
+		  <template #extraItems>
+			<DropdownItem :text="testConnectText" @click.prevent="onClickTestDevice" />
+			<DropdownItem v-if="isShowWriterReview" :text="writerReviewText" @click.prevent="onClickWriterReview" />
+		  </template>
+		</UserAvatar>
+	  </template>
+	  <DeviceTester :notJoin="true" ref="deviceTesterRef" />
+	</AppHeader>
+  </template>
+  <style src="./header.scss" lang="scss" scoped></style>
+  <script src="./header.ts" lang="ts"></script>
+  
