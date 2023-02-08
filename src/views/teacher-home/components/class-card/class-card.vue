c3b5cd8bf39fc15ce01b3654f21f96e8d8aa3742
Joe's LAB <joevu98@gmail.com>
2023-02-08 10:08:53 +0700

    UI Issue - Multiple loaders

--- src/views/teacher-home/components/class-card/class-card.vue
+++ src/views/teacher-home/components/class-card/class-card.vue
@@ -1,47 +1,43 @@
 <template>
-  <div v-if="loading">
-    <span class="class-group__spin-icon">
-      <Spin tip="Loading..." class="ant-custom-home" />
-    </span>
-  </div>
-  <div class="class-holder" v-else>
-    <div class="class">
-      <BaseCard class="class__size class-block">
-        <div class="h-100">
-          <div class="class__title">
-            <span class="class__title--name">{{ title }}</span>
-            <div class="class__content">
-              <span class="description mr-10">{{ unitText }} {{ unit }}</span>
-              <span class="description">{{ lessonText }} {{ currentLesson }}</span>
-            </div>
-          </div>
-        </div>
-      </BaseCard>
-      <!-- <div class="icon-container">
-        <img class="gallery" src="@/assets/images/image-icon.png" />
-        <span class="gallery__text">{{ galleryText }}</span>
-      </div> -->
-    </div>
-    <div class="class-group">
-      <div v-if="groups">
-        <BaseCard :class="item.isHighLighted ? 'class-group__size hightlight' : 'class-group__size'" v-for="item in groups" v-bind:key="item">
-          <div class="group-content" @click="clickToAccess(item.groupId, schoolId)">
-            <div class="m-10">
-              <h2 class="title">{{ groupText }} - {{ item.groupName }}</h2>
-              <span class="description d-block mb">{{ membersText }}{{ item.studentCount }}</span>
-              <span class="description d-block">{{ nextText }}</span>
-              <span class="description d-block" v-if="item.next">{{ handleDateTime(item.next) }}</span>
-            </div>
-            <div class="icon-view" v-show="item.isCurrentDay && item.startClass">
-              <span class="class-group__play-icon" v-if="clickedGroup === item.groupId && item.studentCount > 0 && loadingStart">
-                <Spin spin type="loading" />
-              </span>
-            </div>
-          </div>
-        </BaseCard>
-      </div>
-    </div>
-  </div>
-</template>
-<style lang="scss" src="./class-card.scss" scoped></style>
-<script lang="ts" src="./class-card.ts"></script>
+	<div class="class-holder">
+	  <div class="class">
+		<BaseCard class="class__size class-block">
+		  <div class="h-100">
+			<div class="class__title">
+			  <span class="class__title--name">{{ title }}</span>
+			  <div class="class__content">
+				<span class="description mr-10">{{ unitText }} {{ unit }}</span>
+				<span class="description">{{ lessonText }} {{ currentLesson }}</span>
+			  </div>
+			</div>
+		  </div>
+		</BaseCard>
+		<!-- <div class="icon-container">
+		  <img class="gallery" src="@/assets/images/image-icon.png" />
+		  <span class="gallery__text">{{ galleryText }}</span>
+		</div> -->
+	  </div>
+	  <div class="class-group">
+		<div v-if="groups">
+		  <BaseCard :class="item.isHighLighted ? 'class-group__size hightlight' : 'class-group__size'" v-for="item in groups" v-bind:key="item">
+			<div class="group-content" @click="clickToAccess(item.groupId, schoolId)">
+			  <div class="m-10">
+				<h2 class="title">{{ groupText }} - {{ item.groupName }}</h2>
+				<span class="description d-block mb">{{ membersText }}{{ item.studentCount }}</span>
+				<span class="description d-block">{{ nextText }}</span>
+				<span class="description d-block" v-if="item.next">{{ handleDateTime(item.next) }}</span>
+			  </div>
+			  <div class="icon-view" v-show="item.isCurrentDay && item.startClass">
+				<span class="class-group__play-icon" v-if="clickedGroup === item.groupId && item.studentCount > 0 && loadingStart">
+				  <Spin spin type="loading" />
+				</span>
+			  </div>
+			</div>
+		  </BaseCard>
+		</div>
+	  </div>
+	</div>
+  </template>
+  <style lang="scss" src="./class-card.scss" scoped></style>
+  <script lang="ts" src="./class-card.ts"></script>
+  
--- src/views/teacher-home/teacher-home.vue
+++ src/views/teacher-home/teacher-home.vue
@@ -1,80 +1,78 @@
 <template>
-  <div class="teacher-page" v-if="policy">
-    <div class="teacher-title__wrapper">
-      <div class="teacher-title">
-        <h1 class="teacher-title__welcome">{{ welcomeText }} {{ username }}</h1>
-        <span class="teacher-title__indicator-out" v-if="classOnline" @click="rejoinClass(classOnline, classOnline.groupId)">
-          <span class="teacher-title__indicator-in"></span>
-        </span>
-      </div>
-      <span class="date-time">{{ now }}</span>
-    </div>
-    <div class="group-class-container" v-show="hasClassesShowUp()">
-      <div class="loading" v-show="loadingInfo">
-        <Spin class="ant-custom-home"></Spin>
-      </div>
-      <div v-if="schoolIds.length > 0">
-        <div class="loading" v-if="loading">
-          <Spin tip="Loading..." class="ant-custom-home"></Spin>
-        </div>
-        <div v-for="schoolId in schoolIds" :key="schoolId">
-          <div :class="['group-class-wrapper', isThreeGroup && 'three-group']">
-            <h2 class="school-name">{{ getSchoolNameBySchoolId(schoolId) }}</h2>
-            <div v-for="campusId in getCampusBySchoolId(schoolId)" :key="campusId">
-              <h3 class="campus">{{ getCampusNameByCampusId(campusId) }}</h3>
-              <div v-for="cl in getDataByCampus(campusId)" :key="cl.classId">
-                <ClassCard
-                  class="card-margin"
-                  :key="cl.classId"
-                  :id="cl.classId"
-                  :title="cl.className"
-                  :description="cl.campusName"
-                  :remoteClassGroups="cl.groups"
-                  :isTeacher="cl.isTeacher"
-                  :schoolName="cl.schoolName"
-                  :schoolId="cl.schoolId"
-                  :loadingStart="loadingStartClass"
-                  :unit="cl.unit"
-                  :lesson="cl.lessonNumber"
-                  v-if="cl.groups.length"
-                  @click-to-access="(groupId: string, schoolId: string) => onClickClass(cl, groupId, schoolId)"
-                />
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-    <Empty v-show="!hasClassesShowUp()" />
-    <DeviceTester
-      :unitInfo="unitInfo"
-      @on-join-session="onStartClass"
-      ref="deviceTesterRef"
-      :loading="popUpLoading"
-      :messageStartClass="messageStartClass"
-      :info-start="infoStart"
-    />
-  </div>
-  <Modal :visible="visible && !policy" :closable="false" :centered="true" :maskClosable="false" :footer="null">
-    <h3>{{ policyTitleModal }}</h3>
-    <p>{{ readPolicy }}</p>
-    <hr />
-    <div class="policy-content">
-      <p>
-        <b>{{ policyTitle }}</b>
-        <br />{{ policySubtitle }}
-      </p>
-      <p>{{ policyText1 }}</p>
-      <p>{{ policyText2 }}</p>
-      <p>{{ policyText3 }}</p>
-      <p>{{ policyText4 }}</p>
-    </div>
-    <Checkbox @change="onAgreePolicy">{{ acceptPolicyText }}</Checkbox>
-    <Row type="flex" justify="end">
-      <Button class="btn-cancel-policy" @click="cancelPolicy">{{ cancelText }}</Button>
-      <Button :disabled="!agreePolicy" type="primary" @click="submitPolicy">{{ submitText }}</Button>
-    </Row>
-  </Modal>
-</template>
-<style lang="scss" scoped src="./teacher-home.scss"></style>
-<script lang="ts" src="./teacher-home.ts"></script>
+	<div class="teacher-page" v-if="policy">
+	  <div class="teacher-title__wrapper">
+		<div class="teacher-title">
+		  <h1 class="teacher-title__welcome">{{ welcomeText }} {{ username }}</h1>
+		  <span class="teacher-title__indicator-out" v-if="classOnline" @click="rejoinClass(classOnline, classOnline.groupId)">
+			<span class="teacher-title__indicator-in"></span>
+		  </span>
+		</div>
+		<span class="date-time">{{ now }}</span>
+	  </div>
+	  <div class="group-class-container" v-show="hasClassesShowUp">
+		<div class="loading" v-show="loading">
+		  <Spin class="ant-custom-home"></Spin>
+		</div>
+		<div v-if="schoolIds.length > 0">
+		  <div v-for="schoolId in schoolIds" :key="schoolId">
+			<div :class="['group-class-wrapper', isThreeGroup && 'three-group']">
+			  <h2 class="school-name">{{ getSchoolNameBySchoolId(schoolId) }}</h2>
+			  <div v-for="campusId in getCampusBySchoolId(schoolId)" :key="campusId">
+				<h3 class="campus">{{ getCampusNameByCampusId(campusId) }}</h3>
+				<div v-for="cl in getDataByCampus(campusId)" :key="cl.classId">
+				  <ClassCard
+					class="card-margin"
+					:key="cl.classId"
+					:id="cl.classId"
+					:title="cl.className"
+					:description="cl.campusName"
+					:remoteClassGroups="cl.groups"
+					:isTeacher="cl.isTeacher"
+					:schoolName="cl.schoolName"
+					:schoolId="cl.schoolId"
+					:loadingStart="loadingStartClass"
+					:unit="cl.unit"
+					:lesson="cl.lessonNumber"
+					v-if="cl.groups.length"
+					@click-to-access="(groupId: string, schoolId: string) => onClickClass(cl, groupId, schoolId)"
+				  />
+				</div>
+			  </div>
+			</div>
+		  </div>
+		</div>
+	  </div>
+	  <Empty v-show="!hasClassesShowUp" />
+	  <DeviceTester
+		:unitInfo="unitInfo"
+		@on-join-session="onStartClass"
+		ref="deviceTesterRef"
+		:loading="popUpLoading"
+		:messageStartClass="messageStartClass"
+		:info-start="infoStart"
+	  />
+	</div>
+	<Modal :visible="visible && !policy" :closable="false" :centered="true" :maskClosable="false" :footer="null">
+	  <h3>{{ policyTitleModal }}</h3>
+	  <p>{{ readPolicy }}</p>
+	  <hr />
+	  <div class="policy-content">
+		<p>
+		  <b>{{ policyTitle }}</b>
+		  <br />{{ policySubtitle }}
+		</p>
+		<p>{{ policyText1 }}</p>
+		<p>{{ policyText2 }}</p>
+		<p>{{ policyText3 }}</p>
+		<p>{{ policyText4 }}</p>
+	  </div>
+	  <Checkbox @change="onAgreePolicy">{{ acceptPolicyText }}</Checkbox>
+	  <Row type="flex" justify="end">
+		<Button class="btn-cancel-policy" @click="cancelPolicy">{{ cancelText }}</Button>
+		<Button :disabled="!agreePolicy" type="primary" @click="submitPolicy">{{ submitText }}</Button>
+	  </Row>
+	</Modal>
+  </template>
+  <style lang="scss" scoped src="./teacher-home.scss"></style>
+  <script lang="ts" src="./teacher-home.ts"></script>
+  
